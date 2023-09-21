ec2f4f1bf0c302fb7d29565e3234ed268314c8ec
Yowiesenfeld <136192424+Yowiesenfeld@users.noreply.github.com>
2023-09-21 13:21:35 -0400

    inserted data

--- schema.sh
+++ schema.sh
@@ -1,7 +1,7 @@
 
 
 CREATE TABLE department (
-  id INT PRIMARY KEY,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL
 );
 
@@ -11,6 +11,7 @@ CREATE TABLE role (
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
   department_id INT
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
