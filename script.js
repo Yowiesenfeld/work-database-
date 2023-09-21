54fcf05d05ecfc56af0a8b7e9fdfdd7f39b83f9c
Yowiesenfeld <136192424+Yowiesenfeld@users.noreply.github.com>
2023-09-21 19:20:48 -0400

    added comments

--- script.js
+++ script.js
@@ -13,9 +13,10 @@ connection.connect((err) => {
     console.error('Error connecting to the database:', err);
     return;
   }
-  console.log('Connected to the MySQL database');
+  this.log('Connected to the MySQL database');
   startApp();
 });
+// the code above connects my code to mysql
 
 function startApp() {
   inquirer
@@ -34,6 +35,8 @@ function startApp() {
         'Exit',
       ],
     })
+// the code above prompts user to choose what they would like to see
+
      .then((choice) => {
       switch (choice.action) {
         case 'View all departments':
@@ -46,16 +49,16 @@ function startApp() {
           viewEmployees();
           break;
         case 'Add a department':
-          // Implement code to add a department
+          addDepartment();
           break;
         case 'Add a role':
-          // Implement code to add a role
+          addRole();
           break;
         case 'Add an employee':
-          // Implement code to add an employee
+          addEmployee();
           break;
         case 'Update an employee role':
-          // Implement code to update an employee's role
+          updateEmployeeRole();
           break;
         case 'Exit':
           console.log('Goodbye!');
@@ -66,7 +69,7 @@ function startApp() {
       startApp();
     });
 }
-
+// the code above allows the client to add and update the info on the tables
 
 function viewDepartments() {
   connection.query('SELECT * FROM department', (err, results) => {
@@ -112,4 +115,107 @@ function viewEmployees() {
     startApp();
   });
 }
+// the codes above allows clients to view tables they want to
 
+function addDepartment() {
+  inquirer
+    .prompt({
+      type: 'input',
+      name: 'departmentName',
+      message: 'Enter the name of the department:',
+    })
+    .then((answer) => {
+      connection.query('INSERT INTO departments (name) VALUES (?)', [answer.departmentName], (err, results) => {
+        if (err) {
+          console.error('Error adding department:', err);
+          return;
+        }
+        console.log('Department added successfully!');
+        startApp(); // Return to the main menu
+      });
+    });
+}
+
+
+function addRole() {
+  inquirer
+    .prompt({
+      type: 'input',
+      name: 'Name',
+      message: 'Enter the name of the role:',
+    })
+    .then((answer) => {
+      connection.query('INSERT INTO role (name) VALUES (?)', [answer.roleName], (err, results) => {
+        if (err) {
+          console.error('Error adding role:', err);
+          return;
+        }
+        console.log('Role added successfully!');
+        startApp(); // Return to the main menu
+      });
+    });
+}
+
+
+function addEmployee() {
+  inquirer
+    .prompt({
+      type: 'input',
+      name: 'employeeName',
+      message: 'Enter the name of the employee:',
+    })
+    .then((answer) => {
+      connection.query('INSERT INTO employee (name) VALUES (?)', [answer.employeetName], (err, results) => {
+        if (err) {
+          console.error('Error adding employee:', err);
+          return;
+        }
+        console.log('Employee added successfully!');
+        startApp(); // Return to the main menu
+      });
+    });
+}
+
+
+function updateEmployeeRole() {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'employeeId',
+        message: 'Enter the ID of the employee you want to update:',
+      },
+      {
+        type: 'input',
+        name: 'newRoleId',
+        message: 'Enter the ID of the new role for the employee:',
+      },
+    ])
+    .then((answers) => {
+      const employeeId = parseInt(answers.employeeId);
+      const newRoleId = parseInt(answers.newRoleId);
+
+
+      connection.query(
+        'UPDATE employee SET role_id = ? WHERE id = ?',
+        [newRoleId, employeeId],
+        (err, result) => {
+          if (err) {
+            console.error('Error updating employee role:', err);
+            return;
+          }
+
+          if (result.affectedRows === 0) {
+            console.log('No employee found with that ID.');
+          } else {
+            console.log('Employee role updated successfully!');
+          }
+
+          // Return to the main menu
+          startApp();
+        }
+      );
+    });
+}
+
+// the codes above allow clients to add and update the data in the tables
