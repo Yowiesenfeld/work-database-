761b24db062936539d0e468219b62cf4ffee9974
Yowiesenfeld <136192424+Yowiesenfeld@users.noreply.github.com>
2023-09-21 15:58:02 -0400

    added in prompt

--- script.js
+++ script.js
@@ -7,3 +7,30 @@ const connection = mysql.createConnection({
   password: 'Smile123',
   database: 'your_database',
 });
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    return;
+  }
+  console.log('Connected to the MySQL database');
+  startApp();
+});
+
+function startApp() {
+  inquirer
+    .prompt({
+      type: 'list',
+      name: 'action',
+      message: 'What would you like to do?',
+      choices: [
+        'View all departments',
+        'View all roles',
+        'View all employees',
+        'Add a department',
+        'Add a role',
+        'Add an employee',
+        'Update an employee role',
+        'Exit',
+      ],
+    })
