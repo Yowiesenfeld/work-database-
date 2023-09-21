6033e51320454334ad0517577b5f57f0c42ff80a
Yowiesenfeld <136192424+Yowiesenfeld@users.noreply.github.com>
2023-09-21 18:54:49 -0400

    added function to veiw tables

--- script.js
+++ script.js
@@ -34,3 +34,82 @@ function startApp() {
         'Exit',
       ],
     })
+     .then((choice) => {
+      switch (choice.action) {
+        case 'View all departments':
+          viewDepartments();
+          break;
+        case 'View all roles':
+          viewRoles();
+          break;
+        case 'View all employees':
+          viewEmployees();
+          break;
+        case 'Add a department':
+          // Implement code to add a department
+          break;
+        case 'Add a role':
+          // Implement code to add a role
+          break;
+        case 'Add an employee':
+          // Implement code to add an employee
+          break;
+        case 'Update an employee role':
+          // Implement code to update an employee's role
+          break;
+        case 'Exit':
+          console.log('Goodbye!');
+          connection.end(); // Close the database connection
+          return;
+      }
+
+      startApp();
+    });
+}
+
+
+function viewDepartments() {
+  connection.query('SELECT * FROM department', (err, results) => {
+    if (err) {
+      console.error('Error fetching departments:', err);
+      return;
+    }
+
+    console.log('\nAll Departments:\n');
+    console.table(results);
+
+    // Return to the main menu
+    startApp();
+  });
+}
+
+function viewRoles() {
+  connection.query('SELECT * FROM role', (err, results) => {
+    if (err) {
+      console.error('Error fetching roles:', err);
+      return;
+    }
+
+    console.log('\nAll Role:\n');
+    console.table(results);
+
+    // Return to the main menu
+    startApp();
+  });
+}
+
+function viewEmployees() {
+  connection.query('SELECT * FROM employee', (err, results) => {
+    if (err) {
+      console.error('Error fetching employees:', err);
+      return;
+    }
+
+    console.log('\nAll Departments:\n');
+    console.table(results);
+
+    // Return to the main menu
+    startApp();
+  });
+}
+
